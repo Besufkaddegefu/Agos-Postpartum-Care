466ad9dca93b7cf00a9dae6f3c5816878d0b14d4
Besufkaddegefu <135504863+Besufkaddegefu@users.noreply.github.com>
2026-02-12 02:20:19 -0800

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from telegram.ext import (Application, CommandHandler, CallbackQueryHandler,
 import os
 TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "123456789"))  # fallback to dummy
-LOGO_PATH = os.environ.get("LOGO_PATH", "logo.png")
+LOGO_PATH = os.environ.get("LOGO_PATH", "logo.web")
 
 # --- CONVERSATION STATES ---
 (P_TERMS, P_NAME, P_ADDR, P_AGE, P_PHONE, P_EDD, P_W_BEFORE, P_W_NOW,
@@ -904,4 +904,5 @@ if __name__ == '__main__':
 
     print("Agos Bot is live...")
     app.run_polling()
+
 
